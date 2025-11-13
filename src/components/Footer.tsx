78144b614f7de88723cf857663212b60a5499a6c
Matthieu Felten <matthieu.felten22@gmail.com>
2025-11-13 11:52:28 +0100

    update twitch url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,10 +41,7 @@ function Footer() {
                             ></path>
                         </svg>
                     </a>
-                    <a
-                        href="https://www.twitch.tv/mat06mat4842"
-                        target="_blank"
-                    >
+                    <a href="https://www.twitch.tv/MAT06matt" target="_blank">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 64 64"
